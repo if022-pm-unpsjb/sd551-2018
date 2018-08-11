9cfa539b1939e0fb3e2434403cc3240a82a74348
Lucas <lucas.gabriel2993@gmail.com>
2018-08-11 15:18:24 -0300

    Agregado de .gitignore y lógica básica de detección de paginas
    - Agregado de archivo .gitignore para que los modulos de NodeJS instalados no sean subidos al repositorio.
    - Principio de funcionamiento. Al cargar una pagina se detectan los enlaces que contiene y se los enlista.

--- webcrawler/webcrawler-v1.js
+++ webcrawler/webcrawler-v1.js
@@ -1,7 +1,28 @@
 const request = require('request');
 
-request('https://www.google.com.ar', (err, res, body) => {
+var page = 'https://www.google.com.ar';
+
+request(page , (err, response, body) => {
 	        if (err) { return console.log(err); }
-	        console.log(body);
+	        // console.log(body);
+	        console.log("Pagina: " + page);
+	        console.log("Resultado: " + response.statusCode);
+	        console.log("Data: " + getReleatedURLsList(body));
 });
 
+function getReleatedURLsList(body){
+	var hrefPattern = /href="(.*?)"/g;
+
+	var hrefs = body.match(hrefPattern);
+
+	hrefs.forEach(function(element, index, array){
+		element = element.replace(/href=/g, "");
+		element = element.replace(/"/g, "");
+
+		array[index] = element;
+	});
+
+	return hrefs;
+}
+
+// function 

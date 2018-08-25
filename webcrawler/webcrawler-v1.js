406731df7fdc10bed80a4a9d42f40d3a3b41c3c5
Lucas <lucas.gabriel2993@gmail.com>
2018-08-24 23:15:26 -0300

    Funcionamiento recursivo del recorrido de las paginas

--- webcrawler/webcrawler-v1.js
+++ webcrawler/webcrawler-v1.js
@@ -2,30 +2,59 @@ const request = require('request');
 
 var page = 'https://www.google.com.ar';
 
-visitPage(page);
+visitPage(page, "");
 
 function getReleatedURLsList(body){
 	var hrefPattern = /href="(.*?)"/g;
 
 	var hrefs = body.match(hrefPattern);
 
-	hrefs.forEach(function(element, index, array){
-		element = element.replace(/href=/g, "");
-		element = element.replace(/"/g, "");
+	var links = {
+		absolutes : [],
+		relatives : []
+	}
 
-		array[index] = element;
-	});
+	if(hrefs){
+		hrefs.forEach(function(element, index, array){
+			element = element.replace(/href=/g, "");
+			element = element.replace(/"/g, "");
 
-	return hrefs;
+			if(element.startsWith("/") && element.length > 1 && !element.startsWith("//")){
+				links.relatives.push(element);
+			}
+			else{
+				links.absolutes.push(element);	
+			}
+		});
+	}
+
+	return links;
 }
 
-function visitPage(page){
-	request(page , (err, response, body) => {
-	        if (err) { 
-	        	return console.log(err);
-	        }
+function visitPage(rootPage, relative){
+	var page = rootPage + relative;
+
+	request(page, (err, response, body) => {
+					if(err){
+		        console.log("Pagina: " + page);
+		        console.log("Resultado: Error");
+		        console.log("Links Relativos: 0");
+		        console.log("Links Absolutos: 0");
+
+		        return;
+					}
+
+	        var links = getReleatedURLsList(body);
+
 	        console.log("Pagina: " + page);
 	        console.log("Resultado: " + response.statusCode);
-	        console.log("Data: " + getReleatedURLsList(body));
-});
+	        console.log("Links Relativos: " + links.relatives.length);
+	        console.log("Links Absolutos: " + links.absolutes.length);
+
+	        if(links.relatives.length > 0){
+	        	links.relatives.forEach(function(element, index, array){
+							visitPage(rootPage, element);
+						});
+	        }
+	});
 }
